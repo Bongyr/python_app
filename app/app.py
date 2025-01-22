c23a17495fbedc1b37ecb1ed0427127349819817
Bongyr <54564627+Bongyr@users.noreply.github.com>
2025-01-22 16:05:07 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,16 @@
+from prometheus_flask_exporter import PrometheusMetrics
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route('/ping')
 def ping():
     return jsonify(message="pong")
+    
 @app.route('/')
 def home():
     return "Hello, World!"
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True host='0.0.0.0', port=5000)
