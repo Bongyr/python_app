43ee7a57bd35c430497d8a98fc7a78f5097b236f
Bongyr <54564627+Bongyr@users.noreply.github.com>
2025-01-22 15:34:23 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({"message": "pong"}), 200
-
+@app.route('/')
+def home():
+    return "Hello, World!"
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
